ea22d75ae6fff18c842dbe27af6dfb235e1021b8
陳姵如 <chenpeiru@chenpeirudeMacBook-Air.local>
2019-10-21 01:04:17 +0800

    feat: add state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 function App() {
+  state = {
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id:2 content: 'play Mario kart'}
+    ]
+  }
+
   return (
     <div className="App">
- 
+  
     </div>
   );
 }
